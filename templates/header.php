81779c71de17ee00cff29f5e57493c65988b1b61
vitoria <vitoria.lima116@etec.sp.gov.br>
2023-10-03 13:38:59 -0300

    03-10

--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,4 @@
-<link href="static/css/header.css" rel='stylesheet'>
+<link href="/static/css/header.css" rel='stylesheet'>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
@@ -14,7 +14,7 @@
     <nav class="navbar bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-                <img src="static/imagens/logo.png" alt="Logo" width="60" height="60"
+                <img src="/static/imagens/logo.png" alt="Logo" width="60" height="60"
                     class="d-inline-block align-text-top">
                     CarreiraETEC
             </a>
